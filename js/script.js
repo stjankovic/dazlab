1736e67af787394002527cb1c9b5be2adf4e9457
stjankovic <stefanjankovic31@gmail.com>
2019-07-04 10:14:53 +0200

    Mobile 2.0 - Added the jQuery for mobile

--- js/script.js
+++ js/script.js
@@ -6,15 +6,19 @@ let time = 400;
 
 // Collapsed
 let small = 0.02;
+let mobileSmall = 0.02;
 
 //Default width (100 / 8)
 let normal = 0.125;
+let mobileNormal = 0.10;
 
 // Hover width (100 / 8) * 2
 let medium = 0.25;
+let mobileMedium = 0.20;
 
 // Opened item
 let large = 0.70;
+let mobileLarge = 0.65;
 
 // Controls the collapses
 let reset = true;
@@ -30,7 +34,34 @@ else {
 
 //All of the functions for Mobile screens;
 function mobileWidth() {
-
+    $(".item").click(
+        function() {
+            $(".item").children().css({'opacity': 0});
+            $(this).children().css({'opacity': 1});
+        }
+    );
+    $(".item").click(
+        function() {
+            count++;
+            reset = false;
+            console.log(`Count: ${count}`);
+            if(count == 1) {
+                $(".item").stop().animate({height: $(window).height() * mobileSmall }, time - 200);
+                $(this).stop().animate({height: $(window).height() * mobileLarge }, time - 200);
+            }
+            else if(count == 2) {
+                count = 0;
+                $(".item").stop().animate({height: $(window).height() * mobileNormal }, time - 200);
+                $(".item").children().css({'opacity': 0});
+            }
+        }
+    );
+    $(".overlay").click(
+        function() {
+            $(".item").stop().animate({height: $(window).height() * mobileNormal }, time - 200);
+            $(".item").children().css({'opacity': 0});
+        }
+    );
 }
 //All of the functions for Desktop screens;
 function fullWidth() {
