f91cd50f39305bbcda9188f58c914dddcc033743
stjankovic <stefanjankovic31@gmail.com>
2019-07-04 21:51:25 +0200

    Menu 3.0 - Smoother animation

--- js/script.js
+++ js/script.js
@@ -27,6 +27,9 @@ let count = 0;
 // Controls the menu
 let navOpen = false;
 
+//Current angle for the container rotation
+let currentAngle = 0;
+
 // Conditions for the responsive
 if(width >= 800) {
     fullWidth();
@@ -92,7 +95,17 @@ function menuDesktop() {
             
         }
     )
-    
+    $(".menu-button").hover(
+        function() {
+            if(navOpen == false) {
+                reset = true;
+                count = 0;
+                $(".item").animate({width: $(window).width()* normal}, time - 100);
+                $(".item").children().css({'opacity': 0});
+                $(".item").clearQueue();
+            }
+        }
+    )
 }
 function fullWidth() {
 
@@ -145,19 +158,20 @@ function fullWidth() {
 }
 
 function openNav() {
-    AnimateRotate(70);
+    hideItems();
     $(".container").stop().animate({
         width: $(window).width() * 0.2,
-        height: $(window).width() * 0.2,
+        height: $(window).width() * 0.4,
         left: $(window).width() * 0.4,
-        top: $(window).height() * 0.2
+        top: $(window).height() * 0.05
     },
         500
     );
+    
+    AnimateRotate(-90);
    
 }
 function closeNav() {
-   
     AnimateRotate(0);
     $(".container").animate({
         width: $(window).width() * 0.8,
@@ -167,19 +181,48 @@ function closeNav() {
     },
         500
     );
- 
+    showItems();
  }
 
- function AnimateRotate(angle) {
+function AnimateRotate(angle) {
     var $elem = $('.container');
-    $({deg: 0}).animate({deg: angle}, {
-        duration: 1000,
+    $({deg: currentAngle}).animate({deg: angle}, {
+        duration: 500,
         step: function(now) {
             $elem.css({
                 transform: 'rotate(' + now + 'deg)'
             });
         }
+
     });
+    currentAngle = angle;
+}
+
+function showItems() {
+    $(".item1").fadeIn( 500, function() { });
+    $(".item1").animate({width: $(window).width()* normal}, time - 200);
+    
+    $(".item3").fadeIn( 500, function() { });
+    $(".item3").animate({width: $(window).width()* normal}, time - 200);
+    
+    $(".item5").fadeIn( 500, function() { });
+    $(".item5").animate({width: $(window).width()* normal}, time - 200);
+    
+    $(".item7").fadeIn( 500, function() { });
+    $(".item7").animate({width: $(window).width()* normal}, time - 200);
+}
+function hideItems() {
+    $(".item1").animate({ width: 0}, 300);
+    $(".item1").fadeOut( 100, function() { });
+
+    $(".item3").animate({ width: 0}, 300);
+    $(".item3").fadeOut( 100, function() { });
+
+    $(".item5").animate({ width: 0}, 300);
+    $(".item5").fadeOut( 100, function() { });
+
+    $(".item7").animate({ width: 0}, 300);
+    $(".item7").fadeOut( 100, function() { });
 }
 
 console.log(`Made by Stefan Jankovic`);
